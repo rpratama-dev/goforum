c7e5c8002697d0df75fcdd2042af52593a4b8588
rpratama-dev <rpratama.sky@gmail.com>
2023-06-02 15:16:30 +0000

    refactor: add panic handler to the erro handler middleware

--- src/middleware/router-error.middleware.go
+++ src/middleware/router-error.middleware.go
@@ -2,24 +2,21 @@ package middleware
 
 import (
 	"errors"
-	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/rpratama-dev/mymovie/src/utils"
 )
 
 func RouterErrorMiddleware (next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		err := next(c)
+		defer utils.PanicHandler(c, nil)
 
 		// Check if the error is a router not found error
 		if errors.Is(err, echo.ErrNotFound) {
 			// Handle the router not found error
-			return c.JSON(http.StatusNotFound, map[string]interface{}{
-				"message": "Route not found",
-				"data": nil,
-			})
+			panic("You'r route destination doesn't exist")
 		}
-
 		return err
 	}
 }
