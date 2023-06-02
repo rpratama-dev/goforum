a4b94fe36fcb43d347aff5f3259f2a6192b160ab
rpratama-dev <rpratama.sky@gmail.com>
2023-06-02 15:11:07 +0000

    refactor: add panic handle in the auth middleware

--- src/middleware/auth.middleware.go
+++ src/middleware/auth.middleware.go
@@ -16,34 +16,26 @@ const INVALID_SESSION = "Access Denied, token has invalid / expired"
 // Middleware function to check Bearer token and verify using JWT
 func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
+		defer utils.PanicHandler(c, nil)
 		// Get the Authorization header value
 		authHeader := c.Request().Header.Get("Authorization")
 		// Check if the header value is empty or does not start with "Bearer "
 		authHeaders := strings.Split(authHeader, " ")
 		if (len(authHeaders) < 2 || authHeaders[0] != "Bearer" || len(strings.Split(authHeaders[1], ".")) < 3) {
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: "Access Denied, access token required",
-				Data: nil,
-			})
+			panic("Access Denied, access token required")
 		}
 
 		// Extract the token from the header value & Verify the token
 		claims, err := utils.VerifyJWT(authHeaders[1])
 		if err != nil {
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: INVALID_SESSION,
-				Data: nil,
-			})
+			panic(INVALID_SESSION)
 		}
 
 		// Retrieve session
 		var session models.Session
 		err = session.GetSessionById(claims.SessionID)
 		if err != nil {
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: INVALID_SESSION,
-				Data: nil,
-			})
+			panic(INVALID_SESSION)
 		}
 
 		// Check session expiration
@@ -52,26 +44,17 @@ func AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 			session.DeletedName = claims.Name;
 			session.DeletedFrom = "Auth Middleware";
 			session.SoftDelete()
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: INVALID_SESSION,
-				Data: nil,
-			})
+			panic(INVALID_SESSION)
 		}
 
 		// Make sure session still active
 		if (!session.IsActive) {
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: INVALID_SESSION,
-				Data: nil,
-			})
+			panic(INVALID_SESSION)
 		}
 
 		// Make sure user still active
 		if (!session.User.IsActive) {
-			return c.JSON(http.StatusUnauthorized, httpModels.BaseResponse{
-				Message: "Your account inactive, please contact web administrator",
-				Data: nil,
-			})
+			panic("Your account inactive, please contact web administrator")
 		}
 
 		// Store the claims in the context for access in subsequent handlers
