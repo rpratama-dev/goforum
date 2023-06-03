2e0358d7bfb48bb97e6f935afd7954d2dda6997f
rpratama-dev <rpratama.sky@gmail.com>
2023-06-03 05:51:28 +0000

    feat: add feature comment for selected answer

--- src/routes/v1/question/answer.router.go
+++ src/routes/v1/question/answer.router.go
@@ -9,4 +9,6 @@ func AnswerRouter(route *echo.Group)  {
 	route.POST("", controllers.AnswerStore)
 	route.PUT("/:answer_id", controllers.AnswerUpdate)
 	route.POST("/:answer_id/votes", controllers.AnswerVoteStore)
+	route.POST("/:answer_id/comments", controllers.AnswerCommentStore)
+	route.PUT("/:answer_id/comments/:comment_id", controllers.AnswerCommentUpdate)
 }
